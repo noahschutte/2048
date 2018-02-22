a59105e862a1dbffc377aeb5976eecba33a210ea
Jameson Welling <jamowelling@gmail.com>
2018-02-22 09:15:37 -0700

    get scss mostly working, keyframe animations still not working

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,19 @@
 import { Component } from 'react';
 import { Card, Button } from 'semantic-ui-react';
 // import factory from '../ethereum/factory';
-import Layout from '../components/Layout';
+// import Layout from '../components/Layout';
 import { Link } from '../routes';
-
 import BoardView from '../components/BoardView';
+
+import stylesheet from 'styles/index.scss';
 
 class Play extends Component {
 	render() {
 		return (
-			<Layout>
+			<div>
+				<style dangerouslySetInnerHTML={{ __html: stylesheet }} />
 				<BoardView />
-			</Layout>
+			</div>
 		);
 	}
 }
